a276378336535117973d47b8c9cfb536ccfb57fb
mhernandezit <mhernandezit@gmail.com>
2019-02-07 22:55:09 -0500

    linting

--- assignment2.py
+++ assignment2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
-
+"""Assignment 2, building a CSV to Dict parser """
 from urllib2 import Request, urlopen, URLError
 import argparse
 import datetime
@@ -8,25 +8,29 @@ import csv
 import logging
 import sys
 
-def downloadData (url):
+def downloadData(url):
+    """Download the CSV at the url provided, return a CSV reader object"""
     req = Request(url)
     response = urlopen(req)
     return csv.reader(response)
 
-def processData (fileData):
+def processData(fileData):
+    """ Convert a CSV reader object into a Dictionary with the form:
+        { ID : (Name, Birthday) } with Birthday being a datetime object
+    """
     personData = {}
     for i, row in enumerate(fileData):
         try:
-            day,month,year = row[2].split('/')
-            dateData = datetime.datetime(int(year),int(month),int(day))
+            day, month, year = row[2].split('/')
+            dateData = datetime.datetime(int(year), int(month), int(day))
         except ValueError:
-            logger.error("Error processing line #" + str(i) + " for ID # " + row[0])
+            LOGGER.error("Error processing line #" + str(i) + " for ID # " + row[0])
             continue
-        personData[row[0]] = (row[1],dateData)
+        personData[row[0]] = (row[1], dateData)
     return personData
 
 def displayPerson(personData):
-    """​ Print the name and birthday of a given user identified by the input id 
+    """​ Print the name and birthday of a given user identified by the input id
     Person # <id> is <name> with a birthday of <date>"""
     lookupid = 1
     while lookupid > 0:
@@ -34,43 +38,41 @@ def displayPerson(personData):
         try:
             lookupid = int(iterid)
         except ValueError:
-            logger.error("Input must be a number")
+            LOGGER.error("Input must be a number")
             continue
         if iterid in personData:
-            print("Person # {} is {} with a birthday of {}").format(iterid, personData[iterid][0], personData[iterid][1]) 
+            print("Person # {} is {} with a birthday of {}").format(iterid,\
+            personData[iterid][0], personData[iterid][1])
         elif lookupid <= 0:
-            print("Exiting")
+            print "Exiting"
             break
         else:
-            print("No such person")
+            print "No such person"
 
 def logBuilder():
-    logger = logging.getLogger('assignment2')
-    logger.setLevel(logging.DEBUG)
+    """Builds a log file handler that can be referenced through the module"""
+    log = logging.getLogger('assignment2')
+    log.setLevel(logging.DEBUG)
     try:
-        fh = logging.FileHandler('errors.log')
+        logFile = logging.FileHandler('errors.log')
     except IOError:
-        print("Unable to open log file")
-    fh.setLevel(logging.DEBUG)
+        print "Unable to open log file"
+    logFile.setLevel(logging.DEBUG)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    fh.setFormatter(formatter)
-    logger.addHandler(fh)
-    return logger
+    logFile.setFormatter(formatter)
+    log.addHandler(logFile)
+    return log
 
 if __name__ == '__main__':
-    logger = logBuilder()
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--url', help = 'URL to lookup', required=True)
-    args = parser.parse_args()
+    LOGGER = logBuilder()
+    PARSER = argparse.ArgumentParser()
+    PARSER.add_argument('--url', help='URL to lookup', required=True)
+    ARGS = PARSER.parse_args()
     try:
-        csvdata = downloadData(args.url)
+        CSVDATA = downloadData(ARGS.url)
     except URLError:
-        logger.error("Unable to retrieve CSV file")
-        print('Unable to retrieve CSV file')
+        LOGGER.error("Unable to retrieve CSV file")
+        print 'Unable to retrieve CSV file'
         sys.exit()
-    except HTTPError:
-        logger.error("404 error")
-        print('404, destination unreachable')
-        sys.exit()
-    processDict = processData(csvdata)
-    displayPerson(processDict)
+    PROCESSDICT = processData(CSVDATA)
+    displayPerson(PROCESSDICT)
